beb4323bd4025bc69e947e53ac9cdd36d13b44d6
Philipe Santos <philipe_santos@hotmail.com>
2026-04-14 13:34:44 -0300

    feat: adicionado o ScrollToTop

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/common/Navbar/Navbar';
 import Footer from '../components/common/Footer/Footer';
+import ScrollToTop from '../components/common/ScrollToTop/ScrollToTop';
 import styles from './MainLayout.module.css';
 
 export default function MainLayout() {
   return (
     <div className={styles.layout}>
+      <ScrollToTop />
       <Navbar />
       <main className={styles.main}>
         <Outlet />
