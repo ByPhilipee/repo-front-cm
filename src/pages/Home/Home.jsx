7bb56993bf71f9ec9ca6699717891b84317ea6ac
Philipe Santos <philipe_santos@hotmail.com>
2026-04-14 10:20:06 -0300

    feat: removido o trustStrip

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import Hero from '../../components/Home/Hero';
-import TrustStrip from '../../components/Home/TrustStrip';
 import Approach from '../../components/Home/Approach';
 import ServicesSection from '../../components/Home/ServicesSection';
 import Results from '../../components/Home/Results';
@@ -11,7 +10,6 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <Hero />
-      <TrustStrip />
       <Approach />
       <ServicesSection />
       <Results />
